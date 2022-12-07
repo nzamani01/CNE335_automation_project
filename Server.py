21cd867a397217972a5b0269285aaa7fd0e38e95
nzamani01 <105509446+nzamani01@users.noreply.github.com>
2022-12-06 18:36:34 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 import os
+import paramiko
 
 class Server:
     
@@ -9,18 +10,18 @@ class Server:
         self.ping()
 
     def ping(self):
-       
+        # TODO - Use os module to ping the server
         print("The Server IP is", self.server_ip)
-       
-        filepath = os.path.dirname(os.path.abspath(__file__))
-       
-        os.chdir(filepath)        
 
+        filepath = os.path.dirname(os.path.abspath(__file__))
+        #Set the working directory to the path where the python source code is
+        os.chdir(filepath)        
+ 
         pingresult = "pingResults__.txt"
         pingText = 'ping ' + (self.server_ip) + ' > ' + pingresult         
         print("Command sent to the OS system for the ping is: ", pingText)
         response = os.system(pingText) 
-        
+      
         f = open(pingresult, "r")
         if (response == 0):            
             print("Ping Successfull for the server", self.server_ip)            
@@ -28,3 +29,16 @@ class Server:
         else:
             print("Check the server - Unsuccessfull ping response for ", self.server_ip)
             print(f.read())
+            
+
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname='172.31.11.187', username='ubuntu', key_filename='/home/ubuntu/.ssh/newkey.pem')
+
+stdin, stdout, stderr = ssh.exec_command('lsb_release -a')
+
+for line in stdout.read().splitlines():
+    print(line)
+
+ssh.close()
