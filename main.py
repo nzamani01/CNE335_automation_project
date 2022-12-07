4b06c8099242879ca253d47a49afa9ced53e4374
nzamani01 <105509446+nzamani01@users.noreply.github.com>
2022-12-06 18:33:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import server
+from server import server
 
 def print_program_info():
     # TODO - Change your name
@@ -7,6 +7,14 @@ def print_program_info():
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    #The server.Server("34.209.138.165") in this case EC2 IP is 34.209.138.165
-    #The IP address is passed as a string to the Server class in the server.py module     
-    ec2Server = server.Server("34.209.138.165")  
+     
+    ec2Server = server.Server("172.31.11.187")    
+    my_server_ip = "172.31.11.187"
+    my_rsa_key_file = r"C:\Users\nzamani\.ssh\newkey"
+    username = "ubuntu"
+    my_upgrade_command = 'sudo apt update && sudo apt upgrade -y'
+    my_server = Server(my_server_ip, my_rsa_key_file, username, my_upgrade_command)
+    print(my_server.ping())
+    print("Updating server")
+    ssh_result = my_server.upgrade()
+    print(''.join(ssh_result))
